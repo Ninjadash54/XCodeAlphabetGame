d18959190032555914eba3393d2806a7731df9aa
Josh Deva <djoshandrew@gmail.com>
2024-06-26 12:43:14 -0500

    Initial Commit

---
+++ XCodeFGame/XCodeFGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeFGameApp.swift
+//  XCodeFGame
+//
+//  Created by Deva on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct XCodeFGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
