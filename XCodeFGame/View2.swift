7cc235c86ce816ce1d6643200793ae17e7ff587a
Josh Deva <djoshandrew@gmail.com>
2024-06-26 21:08:30 -0500

    added a simon says

---
+++ XCodeFGame/View2.swift
@@ -0,0 +1,18 @@
+//
+//  View2.swift
+//  XCodeFGame
+//
+//  Created by Deva on 6/26/24.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View2()
+}
---
+++ XCodeFGame/View3.swift
@@ -0,0 +1,18 @@
+//
+//  View3.swift
+//  XCodeFGame
+//
+//  Created by Deva on 6/26/24.
+//
+
+import SwiftUI
+
+struct View3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View3()
+}
